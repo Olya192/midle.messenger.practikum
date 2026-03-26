7bdf0e799bdbdc4a36c548eb140dfe19ea5da608
Olya192 <olya192@bk.ru>
2026-03-26 20:12:11 +0400

    исправление 1

--- index.js
+++ index.js
@@ -5,12 +5,4 @@ document.addEventListener('DOMContentLoaded', () => {
     const app = new App()
     app.render()
 })
-
-
-// import Form from "./src/Form";
-
-// const form = new Form();
-// const FormElement = form.element();
-
-// FormElement && document.body.appendChild(FormElement);
  
